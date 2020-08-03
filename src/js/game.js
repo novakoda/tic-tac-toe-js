3d5a1bf6e8d00766ffbe49d2588ffb6fb1b2e6eb
JR Nova <novakoda9@gmail.com>
2020-08-03 11:05:37 -0500

    added board winner function

--- src/js/game.js
+++ src/js/game.js
@@ -6,12 +6,24 @@ const Player = function(name, mark) {
 
 var board = (function(slots) {
 
-
   return {
     full: function() {
-    return slots.every(slot => slot.innerHTML != "");
+      return slots.every(slot => slot.innerHTML != "");
+    },
+    winner: function() {
+      const winningSlots = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+      let winner = "";
+      winningSlots.forEach(function(cond) {
+        if (cond.every(i => slots[i].innerHTML == "X")) {
+          winner = "X";
+        } else if (cond.every(i => slots[i].innerHTML == "O")) {
+          winner = "O";
+        }
+      })
+      return winner;
     }
   }
+
 })(slots);
 
 let player1 = Player('one', 'X');
@@ -23,3 +35,4 @@ console.log(player1.mark);
 console.log(player2.name);
 console.log(player2.mark);
 console.log(board.full());
+console.log(board.winner());
