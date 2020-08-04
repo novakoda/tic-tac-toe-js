ea29cc1ed7b22099ec9b58373d28335eb96a1605
JR Nova <novakoda9@gmail.com>
2020-08-03 20:05:19 -0500

    added game module

--- src/js/game.js
+++ src/js/game.js
@@ -1,30 +1,52 @@
 var slots = Array.from(document.getElementsByClassName("slot"));
-console.log(slots);
-const Player = function(name, mark) {
+var notificationDiv = document.getElementById("notifications");
+
+const Player = (name, mark) => {
   return {name, mark}
 }
 
 var board = (function(slots) {
+  const winner = () => {
+    const winningSlots = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    let winner = false;
+    winningSlots.forEach(function(cond) {
+      if (cond.every(i => slots[i].innerHTML == "X")) {
+        winner = "X";
+      } else if (cond.every(i => slots[i].innerHTML == "O")) {
+        winner = "O";
+      };
+    });
+    return winner;
+  };
 
-  return {
-    full: function() {
-      return slots.every(slot => slot.innerHTML != "");
-    },
-    winner: function() {
-      const winningSlots = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-      let winner = "";
-      winningSlots.forEach(function(cond) {
-        if (cond.every(i => slots[i].innerHTML == "X")) {
-          winner = "X";
-        } else if (cond.every(i => slots[i].innerHTML == "O")) {
-          winner = "O";
-        }
-      })
-      return winner;
-    }
+  const full = () => {
+    return slots.every(slot => slot.innerHTML != "");
   }
 
+  return {full, winner}
+
 })(slots);
+
+var game = (() => {
+
+  const turn = (player) => {
+    slots.forEach((slot, i) => {
+      slot.addEventListener("click",  function() {
+        if (slots[i].innerHTML == "X" || slots[i].innerHTML == "O") {
+          console.log("bad choice");
+          turn(player);
+        } else {
+          slots[i].innerHTML = player.mark;
+        };
+      });
+    });
+
+  };
+
+  return {turn}
+})();
+
+
 
 let player1 = Player('one', 'X');
 let player2 = Player('two', 'O');
